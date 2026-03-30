9c3f50c9c975f061aff7db66cc1b0b6fcce42068
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2026-03-30 15:10:39 -0300

    atividade apagando elementos

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -1,5 +1,41 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	fmt.Println("hello word!")
+
+	var n, quantDeixaramFila int
+	fmt.Scan(&n)
+
+	idPessoasDafila := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&idPessoasDafila[i])
+	}
+
+	fmt.Scan(&quantDeixaramFila)
+
+	idPessoasDeixaramFila := make(map[int]int) // K: [int]; V: int
+
+	for i := 0; i < quantDeixaramFila; i++ {
+		var id int
+		fmt.Scan(&id)
+		idPessoasDeixaramFila[id] = 1
+	}
+
+	pessoasQueFicaramNaFila := make([]int, 0)
+
+	for _, id := range idPessoasDafila {
+		_, existe := idPessoasDeixaramFila[id]
+
+		if !existe {
+			pessoasQueFicaramNaFila = append(pessoasQueFicaramNaFila, id)
+		}
+	}
+
+	fmt.Print(strings.Trim(fmt.Sprint(pessoasQueFicaramNaFila), "[]"), " \n")
+
 }
