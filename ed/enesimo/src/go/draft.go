bdb6e358bcca6d982f1a9c7705580be7334f7850
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-05-18 11:16:39 -0300

    atividades atualizadas

--- ed/enesimo/src/go/draft.go
+++ ed/enesimo/src/go/draft.go
@@ -2,21 +2,41 @@ package main
 
 import "fmt"
 
-func enesimo(n int) int {
+func ehPrimoAux(n int, divisor int) bool {
 
-	if n <= 0 {
-		return 0
+	if divisor == 1 {
+		return true
 	}
 
+	if n%divisor == 0 {
+		return false
+	}
+	return ehPrimoAux(n, divisor-1)
+}
 
+func ehPrimo(n int) bool {
 
+	if n <= 1 {
+		return false
+	}
+
+	return ehPrimoAux(n, n-1)
+}
+
+func encontrarPrimo(n int, atual int) int {
+
+	if ehPrimo(atual) {
+		if n == 1 {
+			return atual
+		}
+		return encontrarPrimo(n-1, atual+1)
+	}
+
+	return encontrarPrimo(n, atual+1)
 }
 
 func main() {
-
 	var n int
 	fmt.Scan(&n)
-	
-    
-    fmt.Println(enesimo(n))
+	fmt.Println(encontrarPrimo(n, 2))
 }
