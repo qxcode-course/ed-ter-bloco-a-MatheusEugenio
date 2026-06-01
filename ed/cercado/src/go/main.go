7a143311d38dd78a5830eced68b2c335ea08e07e
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-06-01 18:54:05 -0300

    atividade cerca de dfs concluída

--- ed/cercado/src/go/main.go
+++ ed/cercado/src/go/main.go
@@ -6,9 +6,35 @@ import (
 	"os"
 )
 
+func buscaDeVizinhos(board [][]byte, l, c int) {
+
+	if l < 0 || l >= len(board) || c < 0 || c > len(board[0]) || board[l][c] != 'O' {
+		return
+	}
+
+	board[l][c] = 'X'
+
+	buscaDeVizinhos(board, l, c+1)
+	buscaDeVizinhos(board, l, c-1)
+	buscaDeVizinhos(board, l-1, c)
+	buscaDeVizinhos(board, l+1, c)
+}
+
 // NÃO ALTERE A ASSINATURA DA FUNÇÃO solve
 func solve(board [][]byte) {
-	_ = board
+
+	numL := len(board)
+	numC := len(board[0])
+
+	for i := 0; i < numL; i++ {
+		for j := 0; j < numC; j++ {
+			if board[i][j] == 'O' {
+				buscaDeVizinhos(board, i, j)
+				return
+			}
+		}
+	}
+
 }
 
 // NÃO ALTERE A MAIN
