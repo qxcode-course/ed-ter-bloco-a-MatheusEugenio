734775a3ccbc46cd6ae2be24d0e5c640ae474c78
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-06-02 18:44:38 -0300

    resolução da questão rotten de busca em profundidade

--- ed/rotten/src/go/draft.go
+++ ed/rotten/src/go/draft.go
@@ -1,5 +1,85 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
+
+type Pos struct {
+	l, c int
+}
+
+func contagemMinutos(grid [][]int, podres []Pos, quantFrescas int, minutos *int) int {
+
+	if quantFrescas == 0 {
+		return 0
+	}
+
+	direcoes := []Pos{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+	for len(podres) > 0 && quantFrescas > 0 {
+
+		*minutos++
+
+		for _, podreAtual := range podres {
+			podres = podres[1:]
+
+			for _, vizinhos := range direcoes {
+
+				nl := podreAtual.l + vizinhos.l
+				nc := podreAtual.c + vizinhos.c
+
+				if nl < len(grid) && nl >= 0 && nc < len(grid[0]) && nc >= 0 && grid[nl][nc] == 1 {
+
+					grid[nl][nc] = 2
+					quantFrescas--
+				}
+			}
+		}
+
+	}
+
+	return quantFrescas
+}
+
+func apodrecedor(grid [][]int) int {
+
+	frescas := 0
+	var filaPodres = []Pos{}
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == 1 {
+				frescas++
+			} else if grid[i][j] == 2 {
+				filaPodres = append(filaPodres, Pos{i, j})
+			}
+		}
+	}
+
+	minutos := 0
+	frescas = contagemMinutos(grid, filaPodres, frescas, &minutos)
+
+	if frescas > 0 {
+		return -1
+	}
+
+	return minutos
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+
+	var l, c int
+	fmt.Scan(&l, &c)
+
+	mat := make([][]int, l)
+
+	for i := 0; i < l; i++ {
+		mat[i] = make([]int, c)
+		for j := 0; j < c; j++ {
+			fmt.Scan(&mat[i][j])
+		}
+	}
+
+	fmt.Println(apodrecedor(mat))
+
 }
