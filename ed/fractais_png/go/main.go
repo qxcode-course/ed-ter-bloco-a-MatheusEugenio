0fb7ed9fd61f9f306861586b084ee06dd3c5b456
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-06-15 19:01:52 -0300

    primeira imagem de fractais concluída

--- ed/fractais_png/go/main.go
+++ ed/fractais_png/go/main.go
@@ -4,20 +4,72 @@ import (
 	"fmt"
 )
 
+func desenhaArvore(caneta *Pen, comprimento float64) {
+
+	if comprimento < 4.5 {
+		return
+	}
+
+	caneta.Walk(comprimento)
+
+	caneta.Left(35)
+	desenhaArvore(caneta, comprimento*0.8)
+	caneta.Right(35)
+
+	caneta.Right(35)
+	desenhaArvore(caneta, comprimento*0.8)
+	caneta.Left(35)
+
+	caneta.Up()
+	caneta.Walk(-comprimento)
+	caneta.Down()
+}
+
+func desenhaArvoreComFruta(caneta *Pen, comprimento float64) {
+
+	if comprimento < 4.5 {
+		return
+	}
+
+	caneta.Walk(comprimento)
+
+	caneta.Left(35)
+	desenhaArvore(caneta, comprimento*0.8)
+
+	caneta.SetRGB(255, 0, 0)
+	caneta.FillCircle(1.5)
+
+	caneta.Right(35)
+
+	caneta.SetRGB(0, 0, 0)
+
+	caneta.Right(35)
+	desenhaArvore(caneta, comprimento*0.8)
+
+	caneta.SetRGB(0, 0, 0)
+
+	caneta.FillCircle(1.5)
+	caneta.Left(35)
+
+	caneta.Up()
+	caneta.Walk(-comprimento)
+	caneta.Down()
+
+}
+
 func main() {
 	pen := NewPen(300, 300)
 	pen.Up()
-	pen.SetPosition(50, 75) //x e y do plano
 	pen.Down()
 
-	lado := 200.0
+	pen.SetLineWidth(0.7)
+	pen.SetHeading(90)
 
-	// distanciaDaQuina := (lado / 2) * 1.414
+	var comprimento float64
+	comprimento = 49
+	desenhaArvore(pen, comprimento)
 
-	for range 4 {
-		pen.Walk(lado)
-		pen.SetHeading(-90) //mudar angulo
-	}
+	// desenhaArvoreComFruta(pen, comprimento)
 
 	pen.SavePNG("tree.png")
 	fmt.Println("PNG file created successfully.")
