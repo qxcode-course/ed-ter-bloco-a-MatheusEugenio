ca5d2bd5f887d4d17cd3ca50d899396459c68602
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-06-23 22:27:01 -0300

    battleship DFS concluída

--- ed/battleship/src/go/main.go
+++ ed/battleship/src/go/main.go
@@ -6,11 +6,55 @@ import (
 	"os"
 )
 
+type Pos struct {
+	l, c int
+}
+
+func preencher(board [][]byte, i int, j int, visitados map[Pos]bool) bool {
+
+	if i < 0 || j < 0 || i >= len(board) || j >= len(board[0]) {
+		return false
+	}
+
+	pos := Pos{i, j}
+	if visitados[pos] || board[i][j] == '.' {
+		return false
+	}
+
+	visitados[pos] = true
+
+	if preencher(board, i, j+1, visitados) ||
+		preencher(board, i, j-1, visitados) ||
+		preencher(board, i+1, j, visitados) ||
+		preencher(board, i-1, j, visitados) {
+		return true
+	}
+
+	return false
+}
+
 // Função que será chamada no LeetCode
 func countBattleships(board [][]byte) int {
-	//
-	_ := board
-	return 0
+
+	num_l := len(board)
+	num_c := len(board[0])
+	visitados := make(map[Pos]bool)
+	navios := 0
+
+	for i := 0; i < num_l; i++ {
+		for j := 0; j < num_c; j++ {
+
+			pos := Pos{i, j}
+			_, exist := visitados[pos]
+
+			if !exist && board[i][j] == 'X' {
+				navios++
+				preencher(board, i, j, visitados)
+			}
+		}
+	}
+
+	return navios
 }
 
 // Não modifique a função main
