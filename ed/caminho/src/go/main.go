0851356a6a539b0407637fe290a45230094f3bce
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-06-24 00:43:19 -0300

    caminho BFS iniciada-incompleta

--- ed/caminho/src/go/main.go
+++ ed/caminho/src/go/main.go
@@ -14,21 +14,52 @@ func (p Pos) getNeig() []Pos {
 	return nil
 }
 
+// está na posição adequada
 func inside(grid [][]rune, pos Pos) bool {
 	nrows := len(grid)
 	ncols := len(grid[0])
 	return pos.l >= 0 && pos.l < nrows && pos.c >= 0 && pos.c < ncols
 }
 
+// verifica se é o caractere referido
 func match(grid [][]rune, pos Pos, char rune) bool {
 	return inside(grid, pos) && grid[pos.l][pos.c] == char
 }
 
-func search(grid [][]rune, startPos Pos, endPos Pos) {
-	_, _, _ = grid, startPos, endPos
-}
+// method principal
+func search(grid [][]rune, startPos Pos, endPos Pos, visitados map[Pos]bool, caminho map[Pos]Pos, fila Queue[Pos]) bool {
 
-func voltar()
+	if !inside(grid, startPos) {
+		return false
+	}
+
+	if match(grid, startPos, '#') || visitados[startPos] {
+		return false
+	}
+
+	if match(grid, startPos, 'F') && startPos == endPos {
+		return true
+	}
+
+	visitados[startPos] = true
+	
+	fila.Enqueue(startPos)
+	caminho[startPos] =  
+	// registrar o nó atual como anterior do vizinho no mapa caminho
+	grid[startPos.l][startPos.c] = '.'
+
+	if search(grid, startPos, endPos, visitados, caminho) ||
+		search(grid, startPos, endPos, visitados, caminho) ||
+		search(grid, startPos, endPos, visitados, caminho) ||
+		search(grid, startPos, endPos, visitados, caminho) {
+		return true
+	}
+
+	visitados[startPos] = false
+	grid[startPos.l][startPos.c] = ' '
+
+	return false
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -62,7 +93,11 @@ func main() {
 		}
 	}
 
-	search(mat, inicio, fim)
+	queue := *NewQueue[Pos]()
+	visitados := make(map[Pos]bool)
+	caminho := make(map[Pos]Pos)
+
+	search(mat, inicio, fim, visitados, caminho, queue)
 
 	for _, line := range mat {
 		fmt.Println(string(line)) // Converte o slice de runes de volta para string para imprimir
