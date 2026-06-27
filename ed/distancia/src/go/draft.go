4fd69b57a692464d55058c38cfead8deca96a7ce
MatheusEugenio <eugeniomatheus977@gmail.com>
2026-06-27 17:52:48 -0300

    distancia de BACTRAKING finalizada

--- ed/distancia/src/go/draft.go
+++ ed/distancia/src/go/draft.go
@@ -4,117 +4,99 @@ import (
 	"fmt"
 )
 
-func buscaParaEsquerda(limite, value int, runes []rune, indexAtual int) bool {
+func buscaParaEsquerdaRec(indiceLimite, value int, runes []rune, indexAtual int) bool {
 
-	cont := 1
-	for i := indexAtual; i >= 0; i-- {
-
-		if cont > limite {
-			return false
-		}
-
-		if runes[i] == rune('0'+value) {
-			return true
-		}
-
-		cont++
-	}
-
-	return false
-}
-
-func buscaParaDireita(limite, value int, runes []rune, indexAtual int) bool {
-
-	cont := 1
-	for i := indexAtual; i < len(runes); i++ {
-
-		if cont > limite {
-			return false
-		}
-
-		if runes[i] == rune('0'+value) {
-			return true
-		}
-
-		cont++
-	}
-
-	return false
-}
-
-func ehValido(runes []rune, pos, limite int, valores []int) bool {
-
-	if pos == len(runes) {
+	if indexAtual < 0 {
 		return false
 	}
 
-	if runes[pos] != '.' {
-		return ehValido(runes, pos+1, limite, valores)
+	if indexAtual < indiceLimite {
+		return false
 	}
 
-	if !buscaParaDireita(limite, valores[i], runes, pos) && !buscaParaEsquerda(limite, valores[i], runes, pos) {
-		runes[pos] = rune('0' + valores[i])
+	if runes[indexAtual] == rune('0'+value) {
+		return true
 	}
 
-	i := 0
-	for i < len(valores) {
-
-		if ehValido(runes, pos, limite, valores) {
-
-		}
-
-		i++
+	if buscaParaEsquerdaRec(indiceLimite, value, runes, indexAtual-1) {
+		return true
 	}
+
+	return false
+}
+
+func buscaParaDireitaRec(indiceLimite, value int, runes []rune, indexAtual int) bool {
+
+	if indexAtual >= len(runes) {
+		return false
+	}
+
+	if indexAtual > indiceLimite {
+		return false
+	}
+
+	if runes[indexAtual] == rune('0'+value) {
+		return true
+	}
+
+	if buscaParaDireitaRec(indiceLimite, value, runes, indexAtual+1) {
+		return true
+	}
+
+	return false
 
 }
 
-func preencher(valoresL []int, str string, limite int) string {
+// se retornar true é pq achou um valor igual ao valor passado em um raio de L
+func achouIgual(indexLimiteDir, indexLimiteEsq int, runes []rune, indexAtual int, value int) bool {
 
-	runes := []rune(str)
+	if buscaParaDireitaRec(indexLimiteDir, value, runes, indexAtual) || buscaParaEsquerdaRec(indexLimiteEsq, value, runes, indexAtual) {
+		return true
+	}
 
+	return false
+}
+
+func preencher(valoresL []int, runes []rune, limite int) (bool, string) {
+
+	indexPonto := -1
 	for i := range runes {
 		if runes[i] == '.' {
-
-			j := 0
-			for j < len(valoresL) {
-
-				if !buscaParaDireita(limite, valoresL[j], runes, i) && !buscaParaEsquerda(limite, valoresL[j], runes, i) {
-
-					runes[i] = rune('0' + valoresL[j])
-					break
-				}
-				j++
-			}
-			//volta e tenta outro caminho
+			indexPonto = i
 		}
 	}
 
-	return string(runes)
+	if indexPonto == -1 {
+		return true, string(runes)
+	}
+
+	limiteDir := indexPonto + limite
+	if limiteDir == len(runes) {
+		limiteDir = len(runes) - 1
+	}
+
+	limiteEsq := indexPonto - limite
+	if limiteEsq < 0 {
+		limiteEsq = 0
+	}
+
+	for _, val := range valoresL {
+
+		if !achouIgual(limiteDir, limiteEsq, runes, indexPonto, val) {
+
+			runes[indexPonto] = rune('0' + val)
+
+			caminhos, _ := preencher(valoresL, runes, limite)
+			if caminhos {
+				return true, string(runes)
+			}
+
+			runes[indexPonto] = '.'
+		}
+	}
+
+	return false, string(runes)
 }
-
-// func preencher(valoresL []int, str string, limite int) string {
-
-// 	runes := []rune(str)
-
-// 	for i := range runes {
-// 		if runes[i] == '.' {
-
-// 			j := 0
-// 			for j < len(valoresL) {
-
-// 				if !buscaParaDireita(limite, valoresL[j], runes, i) && !buscaParaEsquerda(limite, valoresL[j], runes, i) {
-
-// 					runes[i] = rune('0' + valoresL[j])
-// 					break
-// 				}
-// 				j++
-// 			}
-// 			//volta e tenta outro caminho
-// 		}
-// 	}
-
-// 	return string(runes)
-// }
 
 func main() {
 
@@ -130,5 +112,8 @@ func main() {
 		valoresL = append(valoresL, i)
 	}
 
-	fmt.Println(preencher(valoresL, str, limite))
+	runes := []rune(str)
+	_, stringFinal := preencher(valoresL, runes, limite)
+
+	fmt.Println(stringFinal)
 }
